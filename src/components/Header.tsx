3c043e4ec7bc61ee0f7a5338b77247ab38c0349c
Artur Lozovski <arccik@gmail.com>
2023-04-12 19:41:49 +0100

    can add topic, and notes and delete notes

---
+++ src/components/Header.tsx
@@ -0,0 +1,46 @@
+import { signIn, signOut, useSession } from "next-auth/react";
+import React from "react";
+
+export const Header: React.FC = () => {
+  const { data: sessionData } = useSession();
+  console.log("Session ", sessionData);
+  return (
+    <div className="navbar bg-primary text-primary-content">
+      <div className="flex-1">
+        <a className="btn-ghost btn text-xl normal-case">Note GanG</a>
+      </div>
+      {sessionData?.user ? (
+        <div className="flex-none">
+          <div className="dropdown-end dropdown">
+            <label tabIndex={0} className="btn-ghost btn-circle avatar btn">
+              <div className="w-10 rounded-full">
+                {sessionData.user.image && <img src={sessionData.user.image} />}
+              </div>
+            </label>
+            <ul
+              tabIndex={0}
+              className="dropdown-content menu rounded-box menu-compact mt-3 w-52 bg-base-100 p-2 shadow"
+            >
+              <li>
+                <a className="justify-between">
+                  Profile
+                  <span className="badge">New</span>
+                </a>
+              </li>
+              <li>
+                <a>Settings</a>
+              </li>
+              <li>
+                <a onClick={() => signOut()}>Logout</a>
+              </li>
+            </ul>
+          </div>
+        </div>
+      ) : (
+        <button className="btn-primary btn" onClick={() => signIn()}>
+          Login
+        </button>
+      )}
+    </div>
+  );
+};
